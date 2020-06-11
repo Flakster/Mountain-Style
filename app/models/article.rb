391a2e5d6a994980464e02ef846622ccd98284a1
flakster <carlos.santamaria@gmail.com>
2020-06-11 17:18:36 -0500

    Run migrations. Fix error in author foreign key of articles table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :tags, inverse_of: :articles
+  has_many :categories, through: :tags
+  has_many :votes
+  
 end
