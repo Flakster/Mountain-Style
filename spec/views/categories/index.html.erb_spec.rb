27c10947f3883808083c3f8a3a2ceeca90fba7b6
flakster <carlos.santamaria@gmail.com>
2020-06-11 16:58:43 -0500

    Add category scaffold model

---
+++ spec/views/categories/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "categories/index", type: :view do
+  before(:each) do
+    assign(:categories, [
+      Category.create!(
+        name: "Name",
+        priority: 2,
+        article: nil
+      ),
+      Category.create!(
+        name: "Name",
+        priority: 2,
+        article: nil
+      )
+    ])
+  end
+
+  it "renders a list of categories" do
+    render
+    assert_select "tr>td", text: "Name".to_s, count: 2
+    assert_select "tr>td", text: 2.to_s, count: 2
+    assert_select "tr>td", text: nil.to_s, count: 2
+  end
+end
