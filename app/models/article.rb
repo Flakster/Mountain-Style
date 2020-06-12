ee63310f82a1d22782590eceb3429352f59c2d66
flakster <carlos.santamaria@gmail.com>
2020-06-12 10:40:26 -0500

    Add the user by building to the article, fix an error in the association

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@
 
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :tags, inverse_of: :articles
+  has_many :tags, inverse_of: :article
   has_many :categories, through: :tags
   has_many :votes
   validates :title, :body, :image, :author_id, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
-  belongs_to :article
+  has_many :tags
+  has_many :articles, through: :tags
+  validates :name, uniqueness: true, presence: true
 end
