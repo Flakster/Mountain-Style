1b25b51685c4291165353f630298a5184be3083f
flakster <carlos.santamaria@gmail.com>
2020-06-14 21:39:07 -0500

    Add votes to the articles

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,32 @@
 class VotesController < ApplicationController
+  before_action :find_article
+
   def create
+    @vote = current_user.votes.new(article_id: params[:article_id])
+    if @vote.save
+      redirect_to root_path, notice: 'You voted an article.'
+    else
+      redirect_to root_path, alert: 'You cannot vote this post.'
+    end
   end
 
   def destroy
+    vote = Vote.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
+    if vote
+      vote.destroy
+      redirect_to root_path, notice: 'You disvoted an article.'
+    else
+      redirect_to root_path, alert: 'You cannot disvote article that you did not vote before.'
+    end
+  end
+
+
+  private
+
+  def find_article
+    @article = Article.find(params[:article_id])
+    p "=" * 70
+    p "Article  = #{@article.title}"
+    p "=" * 70
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'votes/create'
-  get 'votes/destroy'
   root 'articles#index'
   resources :categories
-  resources :articles
+  resources :votes, only: [:create, :destroy]
+  resources :articles do
+    resources :votes
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
