42a9ffee762c48e4348f2eaa7d6a201b81bae1dc
flakster <carlos.santamaria@gmail.com>
2020-06-15 17:27:18 -0500

    Add style to pages and forms

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,12 +1,15 @@
 class VotesController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_article
 
   def create
     @vote = current_user.votes.new(article_id: params[:article_id])
     if @vote.save
-      redirect_to root_path, notice: 'You voted an article.'
+      flash[:notice] = 'Voted successfully.'
+      redirect_to article_path(params[:article_id])
+      #redirect_to article_path(params[:article_id], notice: 'Voted successfully.')
     else
-      redirect_to root_path, alert: 'You cannot vote this post.'
+      redirect_to root_path
     end
   end
 
@@ -14,9 +17,9 @@ class VotesController < ApplicationController
     vote = Vote.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
     if vote
       vote.destroy
-      redirect_to root_path, notice: 'You disvoted an article.'
+      redirect_to root_path, notice: 'Your vote was removed.'
     else
-      redirect_to root_path, alert: 'You cannot disvote article that you did not vote before.'
+      redirect_to root_path
     end
   end
 
@@ -25,8 +28,6 @@ class VotesController < ApplicationController
 
   def find_article
     @article = Article.find(params[:article_id])
-    p "=" * 70
-    p "Article  = #{@article.title}"
-    p "=" * 70
+
   end
 end
