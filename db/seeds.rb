38e213aac540969c8685b95c445bdb7ed7a7116f
flakster <carlos.santamaria@gmail.com>
2020-06-16 19:57:59 -0500

    Add footer to the articles show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create({name: 'User1', email: 'user1@mail.com', password: '123456'})
-Category.create({name: 'Offgrid', priority: 1})
-Category.create({name: 'Minimalist', priority: 2})
-Category.create({name: 'Chalet', priority: 3})
-Category.create({name: 'Landscape', priority: 4})
-Category.create({name: 'Nature', priority: 5})
-Article.create({title: 'Silence is a treasure', body: 'This is the text for the first article',image: 'default', author_id: 1})
+Category.create({name: 'Minimalist', priority: 1})
+Category.create({name: 'Chalet', priority: 2})
+Category.create({name: 'Landscape', priority: 3})
+Category.create({name: 'Nature', priority: 4})
+Article.create({title: 'Silence is a treasure', body: 'Nam libero tempore, cum soluta nobis est eligendi 
+                                                       optio cumque nihil impedit quo minus id quod maxime
+                                                       placeat facere possimus, omnis voluptas assumenda est,
+                                                       omnis dolor repellendus. Temporibus autem quibusdam et
+                                                       aut officiis debitis aut rerum necessitatibus saepe eveniet
+                                                       ut et voluptates repudiandae sint et molestiae non recusandae.
+                                                       Itaque earum rerum hic tenetur a sapiente delectus, ut aut 
+                                                       reiciendis voluptatibus maiores alias consequatur aut perferendis
+                                                       doloribus asperiores repellat',image: 'default', author_id: 1})
 Tag.create({article_id: 1, category_id: 5}) 
-Vote.create({article_id: 1, user_id: 1})
